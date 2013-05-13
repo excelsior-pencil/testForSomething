f0b3e57e2007ab40fba8dfd5f2627e4edd00584f
Alan <alanlin@AlantekiMacBook-Air-2.local>
2013-05-13 21:34:52 +0800

    project0513
    Created by Alan Lin

---
+++ src/cs/nthu/myproject/TestForSomethingServlet.java
@@ -0,0 +1,13 @@
+package cs.nthu.myproject;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class TestForSomethingServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
