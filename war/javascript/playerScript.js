da776970d06006c5fe52b39d0efba80a1fe76607
Alan <alan5196@icloud.com>
2013-07-07 14:44:17 +0800

    player bug revise
    as title

--- war/javascript/playerScript.js
+++ war/javascript/playerScript.js
@@ -23,7 +23,8 @@
 					      	}
 					      
 					      	// 4. The API will call this function when the video player is ready.
-					    	var videoDuration, durationQuater = 0;
+					    	var videoDuration, durationQuarter = 0;
+					    	var currentDurationRate = 0.1;
 					    	function onPlayerReady(event) {
 					    		event.target.playVideo();
 					    		videoDuration = player.getDuration();
@@ -33,25 +34,33 @@
 						      //    the player should play for six seconds and then stop.
 						      // var done = false;
 						    function onPlayerStateChange(event){
-						    	if (player.getPlayerState() == 2) { // when video is pausing record the duration rate in duration quater
-						    		durationQuater = player.getCurrentTime() / videoDuration;
-						    	}
-						    	drawTheProgress();
+						    	if (player.getPlayerState() == 2 || player.getPlayerState() == 0) { // when video is paused or ended record 
+						    		if( durationQuarter < currentDurationRate){						//the duration rate in durationQuarter
+						    			durationQuarter = currentDurationRate; 						//player.getCurrentTime() / videoDuration;
+							    	}
+							    }
+							    // the following prototyping code change the picture of the left control depending on the duration rate.
+							    // The future will query the server to get the user's learning progress table and show the picture.
+							    if(durationQuarter > 0.9){
+							   		progress.value = 100;
+							   		document.JH_math_1.src = "http://www.veryicon.com/icon/png/System/Red%20CandyBar/Apple%20Logo%20Red.png";
+							   	}
+						      	else if(durationQuarter > 0.75){
+						      		progress.value = 75;
+						      		document.JH_math_1.src = "http://www.iconlet.com/icons/kensaunders/5/1/Apple.png";
+								}
+						      	else if(durationQuarter > 0.5){
+						      		progress.value = 50;
+						      		document.JH_math_1.src ="http://www.iconeasy.com/icon/png/System/Candied%20Apples/Candy%20Apple%20Blue%202.png";
+						      	}
+						      	else if(durationQuarter > 0.25){
+							  		progress.value = 25;
+							  		document.JH_math_1.src ="http://www.iconpng.com/png/phuzion/apple.png";
+						      	}
+						      	else{
+						      		progress.value = 0;
+						      		document.JH_math_1.src ="http://upload.wikimedia.org/wikipedia/zh/6/61/Apple_Safari.png";
+						   		}
+						   		currentDurationRate = player.getCurrentTime() / videoDuration;
+
 						    }
-					      // function rtnDuration() {
-					      //   var videoDur = player.getDuration();
-					      //   alert("Duration is :" + videoDur);
-					      // }
-					      	function drawTheProgress(){
-					      		var a = durationQuater;
-					      		if(a > 0.9)
-					      			progress.value = 100;
-					      		else if(a > 0.75)
-					      			progress.value = 75;
-					      		else if(a > 0.5)
-					      			progress.value = 50;
-					      		else if(a > 0.25)
-					      			progress.value = 25;
-					      		else
-					      			progress.value = 0;
-					      	}					      
